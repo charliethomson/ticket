2ec3b5b31b06acc124b45215218c23193e375541
Charlie Thomson <charliecthomson@gmail.com>
2020-09-18 02:48:14 -0400

    step

---
+++ models.ts
@@ -0,0 +1,60 @@
+
+interface User {
+    name: string,
+    id: number,
+    status: string,
+    ban: boolean,
+    queue: number[], // IDs of active workorders assigned to this user
+}
+
+interface Workorder {
+    customer: Customer,
+    device: Device,
+    status: number, // See STATUS
+    notes: Note[],
+    sale_items: Item[], // TODO: Web assembly u64-ing
+    location: string,
+    id: number,
+
+    is_active: boolean,
+    is_warranty: boolean,
+
+    next_update: Date,
+    last_update: Date,
+    created: Date,
+}
+
+type Item = number; // TODO
+
+interface Note {
+    author: User,
+    created: Date,
+    next_update: Date,
+    status: number,
+    // public: boolean, // TODO
+
+
+}
+
+interface Customer {
+
+}
+interface Device {
+
+}
+
+const STATUS = [
+    'Sale completed',
+    'Device abandoned',
+    'Repaired - RFP',
+    'Unrepairable - RFP',
+    'No problem found - RFP',
+    'Declined - RFP',
+    'Awaiting Diagnostic',
+    'Awaiting Repair',
+    'Awaiting Device',
+    'Awaiting Customer',
+    'Awaiting Technician',
+    'Awaiting Parts',
+    'Need to order parts',
+];
