87681b3c690954966f2f64a9a67fc484d66fa4ee
Charlie Thomson <charliecthomson@gmail.com>
2020-09-22 14:41:24 -0400

    step, untested

--- server/src/db.rs
+++ server/src/db.rs
@@ -1,13 +1,10 @@
 use mongodb::{error::Result as MongoResult, Client, Collection, Database};
 
-const DB_URI: &'static str = "mongodb://localhost:27017";
+const DB_URI: &'static str = "mongodb://charliecthomson.com:27020";
 
-pub async fn get_collection<S: ToString>(
-    database: S,
-    collection_name: S,
-) -> MongoResult<Collection> {
+pub async fn get_collection<S: ToString>(collection_name: S) -> MongoResult<Collection> {
     let client = Client::with_uri_str(DB_URI).await?;
     Ok(client
-        .database(&database.to_string())
+        .database("offsite")
         .collection(&collection_name.to_string()))
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -17,9 +17,30 @@ async fn index() -> impl Responder {
     ""
 }
 
+#[get("/api/users/{id}")]
+async fn get_user(web::Path(id): web::Path<u32>) -> impl Responder {
+    let collection = db::get_collection("users").await.unwrap();
+    let mut filter = mongodb::bson::document::Document::new();
+    filter.insert("id", id);
+    let stream = collection
+        .find(Some(filter), None)
+        .await
+        .unwrap()
+        .next()
+        .await
+        .unwrap()
+        .unwrap();
+    HttpResponse::Ok().json(
+        models::User::deserialize(mongodb::bson::Deserializer::new(
+            mongodb::bson::Bson::Document(stream),
+        ))
+        .unwrap(),
+    )
+}
+
 #[get("/api/workorders/all")]
 async fn workorders_all() -> impl Responder {
-    let collection = db::get_collection("offsite", "workorders").await.unwrap();
+    let collection = db::get_collection("workorders").await.unwrap();
     let items_stream = collection.find(None, None).await.unwrap();
     let items_sync: Vec<mongodb::error::Result<mongodb::bson::document::Document>> =
         items_stream.collect().await;
