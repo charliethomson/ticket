7ce73e7050029ad088a8b8d87d18eac214e56969
Charlie Thomson <charliecthomson@gmail.com>
2020-09-22 15:55:16 -0400

    cleanup

--- server/src/main.rs
+++ server/src/main.rs
@@ -25,15 +25,10 @@ pub struct OkMessage<Message: Serialize> {
     pub message: Option<Message>,
 }
 
-#[get("/")]
-async fn index() -> impl Responder {
-    ""
-}
-
 #[get("/api/users/{id}")]
 async fn get_user(web::Path(id): web::Path<u32>) -> impl Responder {
     let collection = db::get_collection("users").await.unwrap();
-    let mut filter = mongodb::bson::document::Document::new();
+    let mut filter = Document::new();
     filter.insert("id", id);
     let stream = collection
         .find(Some(filter), None)
@@ -43,12 +38,7 @@ async fn get_user(web::Path(id): web::Path<u32>) -> impl Responder {
         .await
         .unwrap()
         .unwrap();
-    HttpResponse::Ok().json(
-        models::User::deserialize(mongodb::bson::Deserializer::new(
-            mongodb::bson::Bson::Document(stream),
-        ))
-        .unwrap(),
-    )
+    HttpResponse::Ok().json(User::deserialize(Deserializer::new(Bson::Document(stream))).unwrap())
 }
 
 #[get("/api/workorders/all")]
@@ -124,79 +114,3 @@ async fn main() -> std::io::Result<()> {
 
     server.run().await
 }
-
-// R/W/R TESTING
-// async fn main() {
-//     let workorder = models::Workorder {
-//         id: 0,
-//         origin: models::Store {
-//             id: 0,
-//             name: String::from("test store"),
-//             contact_name: String::from("test user"),
-//             phone_number: String::from("5555551234"),
-//             workorders: vec![],
-//         },
-//         travel_status: String::from("Delivered"),
-//         delivered_by: String::from("test user"),
-//         created: Utc::now(),
-//         last_update: Utc::now(),
-//         quoted_time: Utc::now(),
-//         status: 0,
-//         customer: models::Customer {
-//             id: 0,
-//             name: String::from("test customer"),
-//             phone_number: String::from("5555554321"),
-//             email: String::from("decline@customer.com"),
-//             devices: vec![],
-//             workorders: vec![],
-//         },
-//         device: models::Device {
-//             id: 0,
-//             serial: String::from("123456789AAA"),
-//             customer: models::Customer {
-//                 id: 0,
-//                 name: String::from("test customer"),
-//                 phone_number: String::from("5555554321"),
-//                 email: String::from("decline@customer.com"),
-//                 devices: vec![],
-//                 workorders: vec![],
-//             },
-//             workorders: vec![],
-//         },
-//         notes: vec![models::Note {
-//             id: 0,
-//             user: models::User {
-//                 id: 0,
-//                 name: String::from("test employee"),
-//                 phone_number: String::from("5555551212"),
-//                 queue: vec![],
-//             },
-//             created: Utc::now(),
-//             next_update: Utc::now(), // TODO ?
-//             contents: String::from("test note 1"),
-//             location: String::from(""),
-//         }],
-//     };
-//     println!("\n\nIN \n{:#?}\n\n", workorder);
-//     let doc = match workorder.into_bson() {
-//         mongodb::bson::Bson::Document(doc) => doc,
-//         _ => panic!(),
-//     };
-//     let collection = db::get_collection("workorders").await.unwrap();
-//     println!("\n\tResult: {:?}", collection.insert_one(doc, None).await);
-//     let out_wo = collection
-//         .find(None, None)
-//         .await
-//         .unwrap()
-//         .collect::<Vec<mongodb::error::Result<mongodb::bson::document::Document>>>()
-//         .await
-//         .iter()
-//         .filter(|res| res.is_ok())
-//         .cloned()
-//         .map(|res| models::Workorder::from(res.unwrap()))
-//         .collect::<Vec<models::Workorder>>();
-//     println!("\n\nOUT \n{:#?}\n\n", out_wo);
-//     let mut doc = mongodb::bson::document::Document::new();
-//     doc.insert("delivered_by", "test user");
-//     println!("REMOVING: {:#?}", collection.delete_many(doc, None).await);
-// }
