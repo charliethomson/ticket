b9d9cc06feee0c073c71a1cc1ef9f182fd3edc1a
Justin Moore <jdmdevelops@gmail.com>
2020-09-28 12:14:51 -0400

    Better code

---
+++ front-end-2.0/src/Components/Create.svelte
@@ -0,0 +1,46 @@
+<script>
+    import Input from "./Input.svelte"
+
+    export let title
+
+    function handleCreate() {
+        alert("Something has been created")
+    }
+</script>
+
+<style>
+    * {
+        color: white;
+        font-family: "Fira Sans", sans-serif;
+        font-size: 33px;
+        font-weight: bold;
+    }
+
+    .create {
+        width: 500px;
+        background-color: #212121;
+        border-radius: 10px;
+        padding: 40px;
+        margin-bottom: 20px;
+    }
+
+    .title {
+        text-align: center;
+    }
+
+    .button {
+        padding: 20px;
+        background-color: #212121;
+        border-radius: 10px;
+        text-align: center;
+    }
+</style>
+
+<div>
+    <div class="create">
+        <div class="title">{title}</div>
+        <slot />
+    </div>
+
+    <div class="button" on:click={handleCreate}><span>Submit </span></div>
+</div>
---
+++ front-end-2.0/src/Components/Input.svelte
@@ -0,0 +1,24 @@
+<script>
+    export let label
+</script>
+
+<style>
+    label {
+        font-weight: normal;
+        font-size: 20px;
+    }
+    input {
+        font-family: inherit;
+        font-size: 1.3rem;
+        font-weight: lighter;
+
+        width: 100%;
+        border: 0;
+        outline: 0;
+        background: transparent;
+        border-bottom: 2px solid white;
+    }
+</style>
+
+<label for="name">{label}:</label>
+<input type="text" />
