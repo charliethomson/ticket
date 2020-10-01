d6ec6e025dad6d09ec2e8153d964185ec9dcde04
Charlie Thomson <charliecthomson@gmail.com>
2020-10-01 15:04:02 -0400

    impl devices new

--- server/src/routes/devices.rs
+++ server/src/routes/devices.rs
@@ -7,15 +7,32 @@ use {
     serde::{Deserialize, Serialize},
 };
 #[derive(Serialize, Deserialize)]
-pub struct DeviceNew {}
-
-#[derive(Serialize, Deserialize)]
-pub struct DeviceUpdate {}
+pub struct DeviceNew {
+    pub serial: String,
+    pub name: String,
+    pub customer_id: i64, // Customer ID
+    pub password: String,
+}
 
 // TODO
 #[post("/api/devices")]
-pub async fn devices_post(Json(_body): Json<DeviceNew>) -> HttpResponse {
-    HttpResponse::Ok().finish()
+pub async fn devices_post(Json(body): Json<DeviceNew>) -> HttpResponse {
+    match Device::insert(&Device {
+        id: 0,
+        serial: body.serial.clone(),
+        name: body.name.clone(),
+        customer_id: body.customer_id,
+        password: body.password.clone(),
+    }) {
+        Ok(id) => HttpResponse::Ok().json(OkMessage {
+            ok: true,
+            message: Some(id),
+        }),
+        Err(e) => HttpResponse::InternalServerError().json(OkMessage {
+            ok: false,
+            message: Some(e.to_string()),
+        }),
+    }
 }
 
 // TODO
@@ -26,6 +43,6 @@ pub async fn devices_get(Json(_filter): Json<DeviceOptions>) -> HttpResponse {
 
 // TODO
 #[put("/api/devices")]
-pub async fn devices_put(Json(_body): Json<DeviceUpdate>) -> HttpResponse {
+pub async fn devices_put(Json(_body): Json<DeviceOptions>) -> HttpResponse {
     HttpResponse::Ok().finish()
 }
