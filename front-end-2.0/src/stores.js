5e0c6170b674aa095f5acb9c4bef99028f07370e
Charlie Thomson <charliecthomson@gmail.com>
2020-11-08 11:04:47 -0500

    remove dead code

--- front-end-2.0/src/stores.js
+++
@@ -1,4 +0,0 @@
-import { writable } from "svelte/store"
-
-export let toggleCreate = writable(false)
-export let toggleSearch = writable(true)
--- front-end/src/stores.js
+++
@@ -1,4 +0,0 @@
-import { writable } from "svelte/store"
-
-export let storeSubmitted = writable(true)
-export let customerSubmitted = writable(true)
