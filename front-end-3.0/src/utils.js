a0dff65792081321379ec149b7a2ba0c52280f36
Tyler Ohlenforst <tohlenforst@Tylers-Mac-Pro.local>
2020-11-26 04:07:05 -0600

    re-organized utils

--- front-end-3.0/src/utils.js
+++ front-end-3.0/src/utils.js
@@ -1,11 +1,11 @@
 const baseUrl = `http://${process.env.isProd ? "offsite.repair" : "localhost:8080"}/api/`
 const delay = x => new Promise(resolve => setTimeout(() => resolve(x), 1000))
 
+// HTTP REQUESTS
 const GET = (url) =>
     fetch(baseUrl + url)
         .then(data => data.json())
         .then(delay)
-
 const POST = async (url, body) => 
     fetch(baseUrl + url, {
         method: "POST",
@@ -17,12 +17,17 @@ const POST = async (url, body) =>
     })
         .then(data => data.json())
         .then(delay)
-
+// REQUEST -> GET ALL
 export const getWorkorders = async () => await GET('workorders')
 export const getDevices = async () => await GET('devices')
-export const getCustomer = async () => await GET('customer')
+export const getCustomers = async () => await GET('customer')
 export const getStores = async () => await GET('stores')
 export const getNotes = async () => await GET('notes')
-
+// REQUEST -> CREATE
 export const createNote = async (body) => await POST('notes', body)
-
+// REQUEST -> GET ONE
+export const getWorkorder = async (id) => GET('workorders').then(workorders => workorders[id])
+export const getDevice = async (id) => await GET('devices').then(devices => devices[id])
+export const getCustomer = async (id) => await GET('customer').then(customers => customers[id])
+export const getStore = async (id) => await GET('stores').then(stores => stores[id])
+export const getNote = async (id) => await GET('notes').then(notes => notes[id])
