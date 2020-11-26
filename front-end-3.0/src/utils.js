2d173ce8bdb7cca22c1da5472d48d1eb8d6a20e1
Tyler Ohlenforst <tohlenforst@Tylers-Mac-Pro.local>
2020-11-26 03:43:10 -0600

    added post requests and delays

--- front-end-3.0/src/utils.js
+++ front-end-3.0/src/utils.js
@@ -1,13 +1,28 @@
 const baseUrl = `http://${process.env.isProd ? "offsite.repair" : "localhost:8080"}/api/`
+const delay = x => new Promise(resolve => setTimeout(() => resolve(x), 1000))
 
-const GET = async (url) => {
-    const response = await fetch(baseUrl + url)   
-    const data = await response.json()
-    return data
-}
+const GET = (url) =>
+    fetch(baseUrl + url)
+        .then(data => data.json())
+        .then(delay)
+
+const POST = async (url, body) => 
+    fetch(baseUrl + url, {
+        method: "POST",
+        mode: "cors",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+    })
+        .then(data => data.json())
+        .then(delay)
 
 export const getWorkorders = async () => await GET('workorders')
 export const getDevices = async () => await GET('devices')
 export const getCustomer = async () => await GET('customer')
 export const getStores = async () => await GET('stores')
 export const getNotes = async () => await GET('notes')
+
+export const createNote = async (body) => await POST('notes', body)
+
